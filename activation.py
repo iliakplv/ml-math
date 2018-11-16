7ff4b44e2b119928179fbd28fc1ecba31a4e9128
Ilia Kopylov <ilia@springload.co.nz>
2018-11-16 15:29:08 +1300

    Implement derivatives

--- activation.py
+++ activation.py
@@ -10,7 +10,23 @@ def tanh(x):
     return algebra.Vector([math.tanh(item) for item in x.vector])
 
 
+def relu_derivative(x):
+    return algebra.Vector([1.0 if item > 0 else 0 for item in x.vector])
+
+
+def tanh_derivative(x):
+    t = tanh(x)
+    t_sqr_list = algebra.list_mul(t.vector, t.vector)
+    return algebra.Vector([1.0 - item for item in t_sqr_list])
+
+
 if __name__ == '__main__':
-    relu(algebra.Vector([-0.2, -0.1, 0.0, 1.0, 2.0])).print()
+    v = algebra.Vector([-0.2, -0.1, 0.0, 1.0, 2.0, 8.0])
+    relu(v).print()
+    relu_derivative(v).print()
+
     print()
-    tanh(algebra.Vector([0.0, 0.5, 1.0, 5.0])).print()
+
+    v = algebra.Vector([0.0, 0.5, 1.0, 2.0, 8.0])
+    tanh(v).print()
+    tanh_derivative(v).print()
