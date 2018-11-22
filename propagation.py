1aff97e976870d39fcf5d948e771ee0f4e589640
Ilia Kopylov <iliakplv@gmail.com>
2018-11-22 23:29:37 +1300

    Test layer back prop

--- propagation.py
+++ propagation.py
@@ -18,13 +18,29 @@ def layer_back_prop(dA_curr, W_curr, Z_curr, A_prev, act_back, batch_size):
 
 
 if __name__ == '__main__':
-    input = algebra.Vector([0.1, 0.2])
-    weights = algebra.Matrix([[1, 2], [2, 1]])
-    bias = algebra.Vector([1, 1])
     act_fun = activation.tanh
+    act_back = activation.tanh_back
 
-    z, a = layer_forward_prop(input, weights, bias, act_fun)
-    z.print()
-    a.print()
+    A_prev = algebra.Vector([0.1, 0.2])
+    W_curr = algebra.Matrix([
+        [0.5, 0.6],
+        [0.6, 0.7],
+        [0.6, 0.5]])
+    b_curr = algebra.Vector([0.4, 0.5, 0.5])
 
-    # todo test layer_back_prop
+    print('\nForward prop (layer)')
+    Z_curr, A_curr = layer_forward_prop(A_prev, W_curr, b_curr, act_fun)
+    print('A_curr:')
+    A_curr.print()
+
+    print('\nBack prop (layer)')
+    layer_gradient = algebra.Vector([0.1, 0.2, 0.1])
+
+    dW_curr, db_curr, dA_prev = layer_back_prop(layer_gradient, W_curr, Z_curr, A_prev, act_back, 1)
+
+    print('dW_curr:')
+    dW_curr.print()
+    print('db_curr:')
+    db_curr.print()
+    print('dA_prev:')
+    dA_prev.print()
