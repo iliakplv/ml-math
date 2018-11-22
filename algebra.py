c3401893381b005841269f149ac226b91ce29f5d
Ilia Kopylov <iliakplv@gmail.com>
2018-11-22 20:02:15 +1300

    Implement more algebra

--- algebra.py
+++ algebra.py
@@ -1,12 +1,30 @@
+def list_sum(l):
+    result = 0
+    for item in l:
+        result += item
+    return result
+
+
 def list_mul(l1, l2):
     return [l1[i] * l2[i] for i in range(len(l1))]
 
 
-def list_sum(l):
-    sum = 0
-    for item in l:
-        sum += item
-    return sum
+def mat_mul(matrix1, matrix2):
+    matrix1rows = len(matrix1)
+    matrix1cols = len(matrix1[0])
+    matrix2cols = len(matrix2[0])
+
+    result = []
+    for row in range(matrix1rows):
+        result_line = []
+        for col in range(matrix2cols):
+            cross_sum = 0
+            for sum_index in range(matrix1cols):
+                cross_sum += matrix1[row][sum_index] * matrix2[sum_index][col]
+            result_line.append(cross_sum)
+        result.append(result_line)
+
+    return result
 
 
 class Vector:
@@ -24,7 +42,21 @@ class Vector:
         return Vector([self.vector[i] + other.vector[i] for i in range(len(self))])
 
     def __mul__(self, other):
+        if len(self) != len(other):
+            raise Exception('Different size vectors')
         return list_sum(list_mul(self.vector, other.vector))
+
+    def mul_element_wise(self, other):
+        if len(self) != len(other):
+            raise Exception('Different size vectors')
+        return Vector(list_mul(self.vector, other.vector))
+
+    def mul_outer(self, other):
+        if len(self) != len(other):
+            raise Exception('Different size vectors')
+        m1 = Matrix([[item] for item in self.vector])
+        m2 = Matrix([other.vector])
+        return m1 * m2
 
     def print(self):
         for item in self.vector:
@@ -56,10 +88,23 @@ class Matrix:
             matrix_t.append(row_t)
         return Matrix(matrix_t)
 
-    def __mul__(self, vector):
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return self.mul_vector(other)
+        elif isinstance(other, Matrix):
+            return self.mul_matrix(other)
+        else:
+            raise NotImplementedError
+
+    def mul_vector(self, vector):
         if self.cols != len(vector):
             raise Exception('Incompatible matrix/vector dimensions')
         return Vector([list_sum(list_mul(row, vector.vector)) for row in self.matrix])
+
+    def mul_matrix(self, other):
+        if self.cols != other.rows:
+            raise Exception('Incompatible matrix dimensions')
+        return Matrix(mat_mul(self.matrix, other.matrix))
 
     def print(self):
         for row in self.matrix:
@@ -68,10 +113,12 @@ class Matrix:
 
 if __name__ == '__main__':
     v = Vector([1, 2, 3])
-    print(len(v))
     v.print()
-    print(v * v)
+    print(len(v))
     (v + v).print()
+    print(v * v)
+    v.mul_element_wise(v).print()
+    v.mul_outer(v).print()
 
     m = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     print(m.dimensions())
@@ -81,3 +128,5 @@ if __name__ == '__main__':
     m = Matrix([[1, 1], [1, 2]])
     v = Vector([3, 4])
     (m * v).print()
+
+    (m * m).print()
